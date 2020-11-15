43efaec5a187f36cc0ee2fd2151116e5d6a283e2
Ernesto Colmenarez <vdv174@gmail.com>
2020-11-15 11:27:13 -0500

    [Create Project] Initial Commit

---
+++ meli-mobile-challenge/meli-mobile-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meli-mobile-challenge
+//
+//  Created by Ernesto Colmenarez on 15/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ meli-mobile-challenge/meli-mobile-challengeTests/meli_mobile_challengeTests.swift
@@ -0,0 +1,33 @@
+//
+//  meli_mobile_challengeTests.swift
+//  meli-mobile-challengeTests
+//
+//  Created by Ernesto Colmenarez on 15/11/20.
+//
+
+import XCTest
+@testable import meli_mobile_challenge
+
+class meli_mobile_challengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
